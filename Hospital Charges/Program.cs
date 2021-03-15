1c126b3d38b0ffbb69c7588804cb1fb98c886ae1
Darren Phillips <dzp0067@auburn.edu>
2021-03-15 15:43:02 -0500

    Created prompts for user input

--- Hospital Charges/Program.cs
+++ Hospital Charges/Program.cs
@@ -16,6 +16,18 @@ namespace Hospital_Charges
             int surgicalCharges;
             int labFees;
             int rehabCharges;
+
+            System.Console.WriteLine("Please enter the following information for you hospital stay.");
+            System.Console.WriteLine("Number of days spent in the hospital: ");
+            daysIn = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Total charges for medicine: ");
+            medCharges = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Total surgical charges: ");
+            surgicalCharges = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Total lab fees: ");
+            labFees = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Total physical rehab charges: ");
+            rehabCharges = Convert.ToInt32(Console.ReadLine());
         }
 
         public static int CalcStayCharges (int daily, int days)
